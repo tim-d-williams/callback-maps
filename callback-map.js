8ee453c59e83822fd8b69e5d73c0126bc2ec19d5
Tim Williams <timwilliams.txW@gmail.com>
2019-01-13 02:16:02 +0000

    create own map function with callback

---
+++ callback-map.js
@@ -0,0 +1,20 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+function map(words, callBack) {
+  newArray = [];
+  for (i = 0; i < words.length; i++) {
+    newArray.push(callBack(words[i]));
+  } return newArray;
+}
